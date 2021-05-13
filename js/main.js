9515bcce021a7f35e49fe43e780429223aaa9f76
Lorenz Marqueses <83505132+ap-agustin@users.noreply.github.com>
2021-05-13 22:49:30 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ const galleryImages = [
   "img/media/pollution.jpg",
   "img/media/poverty.jpg",
   "img/media/missingout.jpg",
-  "img/media/noonngayon.jpg"
+  "img/media/noonngayon.jpg",
+  "img/media/dyaryo.jpg"
 ]; // replace with actual urls
 const galleryTitles = [
   "Obra",
@@ -18,7 +19,8 @@ const galleryTitles = [
   "Tainting Beauty",
   "Bliss in Tribulation",
   "Missing Out",
-  "Kalikasan : Ang Magkabilang Panig"
+  "Kalikasan : Ang Magkabilang Panig",
+  "Dyaryo"
 ];
 const galleryAuthors = [
   "James F. Baldemoro",
@@ -29,18 +31,20 @@ const galleryAuthors = [
   "Naomi Zoe M. Isoga and Angelica Cargado",
   "Naomi Zoe M. Isoga and Angelica Cargado",
   "Jules Michael S. Bala",
-  "Christine T. Zaragoza"
+  "Christine T. Zaragoza",
+  "Rain Darla Angeliz Alcaraz"
 ];
 const galleryCaptions = [
   "In omnibus caritas.",
   "Libertas perfundet omnia luce.",
   "きえたい。",
   "Look behind you.",
-  "\"The environment is where we all meet; where we all have a mutual interest; it is the one thing all of us share.\"<br /><span style='font-size: 18px'>-Lady Bird Johnson</span>",
-  "\"Only we humans make waste that nature can't digest.\"<br /><span style='font-size: 18px'>-Charles Moore</span>",
-  "\"To stand at ease in the midst of tribulation, shows a veteran spirit, long experience, and much grace.\"<br /><span style='font-size: 18px'>-Charles Spurgeon</span>",
-  "\"When the productive lands lose their essence, our productive lives shall least have essence!\"<br /><span style='font-size: 18px'>-Ernest Agyemang Yeboah</span>",
-  "Noon at ngayon."
+  "\"The environment is where we all meet; where we all have a mutual interest; it is the one thing all of us share.\"<br /><span style='font-size: 18px'>- Lady Bird Johnson</span>",
+  "\"Only we humans make waste that nature can't digest.\"<br /><span style='font-size: 18px'>- Charles Moore</span>",
+  "\"To stand at ease in the midst of tribulation, shows a veteran spirit, long experience, and much grace.\"<br /><span style='font-size: 18px'>- Charles Spurgeon</span>",
+  "\"When the productive lands lose their essence, our productive lives shall least have essence!\"<br /><span style='font-size: 18px'>- Ernest Agyemang Yeboah</span>",
+  "Noon at ngayon.",
+  "\"At the end of the day, the goals are simple: Safety and security.\"<br /><span style='font-size: 18px'>- Jodi Rell, former Governor of Connecticut</span>"
 ];
 const galleryDescriptions = [
 
@@ -63,7 +67,9 @@ const galleryDescriptions = [
   "This picture shows how this man can find happiness and thank God in poverty. Based on research people who live in poverty have lower life satisfaction or that people in poverty struggle to find happiness. Some say that money affects a person’s happiness but don’t you think that there are more factors to happiness than just money. People can still find happiness without having much money and we can see that in this picture.",
 
   "Deforestation is also the one why we have climate change in our environment. In this picture I want to show that we have only one \"missing out\" what i mean is that we should not forget our environment, because these days we always focus on improving so that's why some of our trees have \"missing out\". My point on this photograph is to let people realise that we should take care of our environment so that no one will be \"missing out.\"",
-  "Makikita sa litrato ang malaking pagkakaiba ng dalawang panig. Sa kaliwang bahagi, makikita ang maaliwalas at makulay na kaligiran. Ang parteng ito ay nagrerepresenta ng sagana at kaaya-ayang kalagayan ng kalikasan noong mga panahong hindi pa nalilikha ang mga bagay na nakasasama ang epekto sa kapaligiran na hanggang ngayon ay kinakaharap pa rin sa kasalukuyan kahit saan mang panig ng mundo.<p>Sa kanang bahagi naman ng litrato, makikita ang malamya at tila malungkot na kaligiran. Matatagpuan rin sa larawan ang parte ng halamang natuyo o patay na. Nirerepresenta nito ang pagkasira at patuloy na pagkasira ng kalikasan gawa ng mga kilusan o aktibidad ng mga mamamayan.</p><p>Ipinapakita ng larawang ito ang kahalagahan ng bawat kilos na ginagawa natin. Ipinapaalala nito na hindi lahat ng gawaing pantao ay maganda para sa kalikasan. Ang iba sa mga ito ay dahilan ng patuloy na pagbagsak at pagkasira ng ating mundong tinitirhan. Hinihikayat ang lahat na makilahok sa mga organisasyon at kilusan na naglalayong mapabuti at maibalik sa dati ang kagandahan ng kapaligiran.</p>"
+  "Makikita sa litrato ang malaking pagkakaiba ng dalawang panig. Sa kaliwang bahagi, makikita ang maaliwalas at makulay na kaligiran. Ang parteng ito ay nagrerepresenta ng sagana at kaaya-ayang kalagayan ng kalikasan noong mga panahong hindi pa nalilikha ang mga bagay na nakasasama ang epekto sa kapaligiran na hanggang ngayon ay kinakaharap pa rin sa kasalukuyan kahit saan mang panig ng mundo.<p>Sa kanang bahagi naman ng litrato, makikita ang malamya at tila malungkot na kaligiran. Matatagpuan rin sa larawan ang parte ng halamang natuyo o patay na. Nirerepresenta nito ang pagkasira at patuloy na pagkasira ng kalikasan gawa ng mga kilusan o aktibidad ng mga mamamayan.</p><p>Ipinapakita ng larawang ito ang kahalagahan ng bawat kilos na ginagawa natin. Ipinapaalala nito na hindi lahat ng gawaing pantao ay maganda para sa kalikasan. Ang iba sa mga ito ay dahilan ng patuloy na pagbagsak at pagkasira ng ating mundong tinitirhan. Hinihikayat ang lahat na makilahok sa mga organisasyon at kilusan na naglalayong mapabuti at maibalik sa dati ang kagandahan ng kapaligiran.</p>",
+
+  "Pinapakita sa larawang ito ang ilang pagsubok na kinakaharap ng ating mundo. Covid-19 ang pangunahing pinagmulan ng kasalukuyang suliranin na hanggang ngayon ay ginagawan pa rin ng solusyon ng gobyerno at ng iba pang mga indibidwal. Ayon dito sa larawan ay nakasaad ang isa pang halimbawa, ito ay ang tinatawag India Variant na kumalat na rin sa 44 na bansa at patuloy na bumibilis ang pagdami ng nagkakaroon nito. Hanggang sa kasalukuyan ay pinagaaralan na rin ang gamot na makakasugpo sa uri na sakit na ito.<p>Ngunit sa aking pananaw, higit na makakatulong kung ang disiplina ay magsisimula sa bawat isa sa atin. Sa simpleng pag-sunod sa mga pinapalaganap na protokol ay malaking tulong na para sa ating lahat na makabawas sa dagdag-isipin sa mga taong naatasan o mas responsable sa pag-lutas ng mga ganitong suliranin kaya't kahit sa simpleng bagay ay piliin pa rin nating tumulong dahil para rin ito sa ikabubuti ng lahat.</p>"
 ];
 const divTradArt = document.getElementById("divTradArt");
 const tradArt = document.getElementById("tradArt");
